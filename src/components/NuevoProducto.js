3e5df17bef537ba512ef48a4f4fbfa6977d688d8
espisepi <sepinaco@gmail.com>
2021-06-02 20:55:49 +0200

    300. Definiendo el action y utilizando useDispatch

--- src/components/NuevoProducto.js
+++ src/components/NuevoProducto.js
@@ -1,6 +1,30 @@
-import React from 'react'
+import React, { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+// Actions de Redux
+import { crearNuevoProductoAction } from '../actions/productoActions';
 
 const NuevoProducto = () => {
+
+    /* utilizar use dispatch y te crea una funcion */
+    const dispatch = useDispatch();
+
+    /* mandar a llamar el action de productoAction */
+    const agregarProducto = () => dispatch( crearNuevoProductoAction() );
+
+    /* Cuando el usuario haga submit */
+    const submitNuevoProducto = useCallback( (e) => {
+            e.preventDefault();
+
+            /* validar formulario */
+
+            /* si no hay errores */
+
+            /* crear el nuevo producto */
+            agregarProducto();
+
+        }, [] )
+
     return (
         <div className="row justify-content-center">
             <div className="col-md-8">
@@ -10,7 +34,9 @@ const NuevoProducto = () => {
                             Agregar Nuevo Producto
                         </h2>
 
-                        <form>
+                        <form
+                            onSubmit={submitNuevoProducto}
+                        >
                             <div className="form-group">
                                 <label>Nombre Producto</label>
                                 <input 
