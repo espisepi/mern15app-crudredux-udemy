4335086884a04bbe180f31838f99b145ccd0bfd3
espisepi <sepinaco@gmail.com>
2021-06-03 23:34:57 +0200

    312. Creando el action para eliminar producto

--- src/reducers/productosReducer.js
+++ src/reducers/productosReducer.js
@@ -4,7 +4,10 @@ import {
     AGREGAR_PRODUCTO_ERROR,
     COMENZAR_DESCARGA_PRODUCTOS,
     DESCARGA_PRODUCTOS_EXITO,
-    DESCARGA_PRODUCTOS_ERROR
+    DESCARGA_PRODUCTOS_ERROR,
+    OBTENER_PRODUCTO_ELIMINAR,
+    PRODUCTO_ELIMINADO_EXITO,
+    PRODUCTO_ELIMINADO_ERROR
 } from '../types';
 
 
@@ -12,7 +15,8 @@ import {
 const initialState = {
     productos: [],
     error: null,
-    loading: false
+    loading: false,
+    productoeliminar: null
 }
 
 export default function(state = initialState, action) {
@@ -42,6 +46,11 @@ export default function(state = initialState, action) {
                 loading: false,
                 error: null,
                 productos: action.payload
+            }
+        case OBTENER_PRODUCTO_ELIMINAR:
+            return {
+                ...state,
+                productoeliminar: action.payload
             }
         default:
             return state;
