0867f69b5f02d5e729737f6253a5891217604fb6
espisepi <sepinaco@gmail.com>
2021-06-03 23:57:19 +0200

    314. Agregando sweetalert a eliminar producto

--- src/components/Producto.js
+++ src/components/Producto.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 // Redux
 import { useDispatch } from 'react-redux';
@@ -13,9 +14,21 @@ const Producto = ({producto}) => {
     // Confirmar si desea eliminarlo
     const confirmarEliminarProducto = id => {
         // preguntar al usuario
-
-        // pasarlo al action
-        dispatch( borrarProductoAction(id) )
+        Swal.fire({
+            title: '¿Estas seguro?',
+            text: "Un producto que se elimina no se puede recuperar",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si, Eliminar!',
+            cancelButtonText: 'Cancelar'
+        }).then((result) => {
+            if (result.value) {
+                // pasarlo al action
+                dispatch( borrarProductoAction(id) );
+            }
+        });
     }
 
     return (
