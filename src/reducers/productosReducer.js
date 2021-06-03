15a36149f0ab840d4d2bebde97c8d22e4caa1b34
espisepi <sepinaco@gmail.com>
2021-06-03 22:57:48 +0200

    309. Obteniendo los Productos en el State

--- src/reducers/productosReducer.js
+++ src/reducers/productosReducer.js
@@ -2,7 +2,9 @@ import {
     AGREGAR_PRODUCTO,
     AGREGAR_PRODUCTO_EXITO,
     AGREGAR_PRODUCTO_ERROR,
-    COMENZAR_DESCARGA_PRODUCTOS
+    COMENZAR_DESCARGA_PRODUCTOS,
+    DESCARGA_PRODUCTOS_EXITO,
+    DESCARGA_PRODUCTOS_ERROR
 } from '../types';
 
 
@@ -28,10 +30,18 @@ export default function(state = initialState, action) {
                 productos: [...state.productos, action.payload]
             }
         case AGREGAR_PRODUCTO_ERROR:
+        case DESCARGA_PRODUCTOS_ERROR:
             return {
                 ...state,
                 loading: false,
                 error: action.payload
+            }
+        case DESCARGA_PRODUCTOS_EXITO:
+            return {
+                ...state,
+                loading: false,
+                error: null,
+                productos: action.payload
             }
         default:
             return state;
